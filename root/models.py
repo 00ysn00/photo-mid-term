02a9793db4f2c99f5cc3faffa27a99b0c7f730a2
ysn <165894576+00ysn00@users.noreply.github.com>
2024-11-24 11:33:36 +0330

    Revert "گروه T4 mudels"

--- root/models.py
+++ root/models.py
@@ -1,25 +1,15 @@
 from django.db import models
-from services.models import Agents
+
 
 
 #complete category-model
 class Category(models.Model):
-    title = models.CharField(max_length=200)
-    status = models.BooleanField(default=False)
+    pass
+
 
 #complete gallery-model
 class Gallery(models.Model):
-    image = models.ImageField(upload_to="gallary",default="default.jpg")
-    category = models.ForeignKey(Category , on_delete=models.CASCADE)
-    title = models.CharField( max_length=200)
-    desc = models.TextField()
-    image_agent = models.ForeignKey(Agents, on_delete=models.CASCADE)
-    cat = models.CharField(max_length=200)
-    client = models.CharField(max_length=200)
-    project_date = models.CharField(max_length=200)
-    url = models.CharField(max_length=200)
-    status = models.BooleanField(default=False)
+    pass
 
-    def __str__(self):
-        return self.title
-    
+
+
--- services/admin.py
+++ services/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import *
 
-
-admin.site.register(Services)
-admin.site.register(Pricing)
-admin.site.register(Ability)
-admin.site.register(Agents)
-
-
-
+# Register your models here.
